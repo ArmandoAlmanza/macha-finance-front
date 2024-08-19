56e4a0d5a6d7107ef6d32a94267d576578499e51
ArmandoAlmanza <armando.s.almanza@gmail.com>
2024-08-19 13:57:23 -0600

    add main layout

---
+++ src/components/layouts/MainLayout.tsx
@@ -0,0 +1,14 @@
+import { Outlet } from "react-router";
+import Header from "../header/Header";
+import Footer from "../footer/Footer";
+const MainLayout = () => {
+    return (
+        <>
+            <Header />
+            <Outlet />
+            <Footer />
+        </>
+    );
+};
+
+export default MainLayout;
